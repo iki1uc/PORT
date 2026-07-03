b3df8fb82a155e88fb9717745be3b887012dc9a3
iki1uc <entertrainer@web.de>
2026-07-03 20:35:29 +0200

    Update maxi.js

--- maxi.js
+++ maxi.js
@@ -1,1 +1,16 @@
+// maxi.js — MAXI (2-fach)
 
+export function MAXI(achsen, msg) {
+    const text = String(msg || "");
+
+    return {
+        ok: true,
+        sys: "MAXI",
+        input: text,
+        maxi: {
+            mode1: achsen,
+            mode2: achsen
+        },
+        time: Date.now()
+    };
+}
